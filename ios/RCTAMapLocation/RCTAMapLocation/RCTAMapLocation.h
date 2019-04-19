f8eeeb182e4e96f296ffbb97c5d4fb08ed02c874
winter <winter.wd@qq.com>
2019-04-19 17:36:16 +0800

    add amap framework

---
+++ ios/RCTAMapLocation/RCTAMapLocation/RCTAMapLocation.h
@@ -0,0 +1,13 @@
+//
+//  RCTAMapLocation.h
+//  RCTAMapLocation
+//
+//  Created by winter on 2019/4/19.
+//  Copyright © 2019 tnrn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTAMapLocation : NSObject
+
+@end
